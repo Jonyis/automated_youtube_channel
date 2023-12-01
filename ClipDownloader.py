3d1f1116d70bda234650bca237e698e7f64911fe
joão silva <joao.s.silva99@gmail.com>
2023-12-01 22:37:17 +0000

    ClipDownloader: minor refactor

--- ClipDownloader.py
+++ ClipDownloader.py
@@ -6,22 +6,29 @@ import requests
 from ClipData import ClipData
 
 
-def download_clip(clip_list: List[ClipData], clips_path: str):
-    # Inspired by https://github.com/viniciusenari/twitch-highlights-bot
+def download_clips(clip_list: List[ClipData], clips_path: str):
+    downloaded_clips = []
     for clip in clip_list:
-        index = clip.thumbnail_url.find('-preview')
-        clip_url = clip.thumbnail_url[:index] + '.mp4'
+        downloaded_clips.append(download_clip(clip, clips_path))
 
-        r = requests.get(clip_url)
-        path = clips_path + '/' + clip.slug + '.mp4'
-        if r.headers['Content-Type'] == 'binary/octet-stream':
-            if not os.path.exists(clips_path):
-                os.makedirs(clips_path)
-            with open(path, 'wb') as f:
-                f.write(r.content)
-            clip.path = path
-        else:
-            print(f'Failed to download clip from thumb: {clip.url}')
+    return downloaded_clips
 
-    return clip_list
+
+def download_clip(clip: ClipData, clips_path: str):
+    # Inspired by https://github.com/viniciusenari/twitch-highlights-bot
+    index = clip.thumbnail_url.find('-preview')
+    clip_url = clip.thumbnail_url[:index] + '.mp4'
+
+    r = requests.get(clip_url)
+    path = clips_path + '/' + clip.slug + '.mp4'
+    if r.headers['Content-Type'] == 'binary/octet-stream':
+        if not os.path.exists(clips_path):
+            os.makedirs(clips_path)
+        with open(path, 'wb') as f:
+            f.write(r.content)
+        clip.path = path
+    else:
+        print(f'Failed to download clip from thumb: {clip.url}')
+
+    return clip
 
